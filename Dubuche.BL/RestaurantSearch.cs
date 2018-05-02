f2ad785af20de50c8c38d3940e112f446999ffe6
Christian Yves Dubuche <dubuche@gmail.com>
2018-05-02 01:13:41 -0400

    Reorganizing classes

--- Dubuche.BL/RestaurantSearch.cs
+++ Dubuche.BL/RestaurantSearch.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dubuche.DAL;
 
 namespace Dubuche.BL
 {
@@ -10,3 +11,12 @@ namespace Dubuche.BL
     {
     }
 }
+//Basic Requirements
+// ## functionality
+//* display top 3 restaurants by average rating
+//* display all restaurants
+//* should allow more than one method of sorting
+//* display details of a restaurant
+//* display all the reviews of a restaurant
+//* search restaurants(e.g.by partial name), and display all matching results
+//* quit application
--- Dubuche.BL/ReviewDisplays.cs
+++ Dubuche.BL/ReviewDisplays.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dubuche.DAL;
 
 namespace Dubuche.BL
 {
-    class ReviewDisplays
+    public class ReviewDisplays
     {
-
+        //public static Reviewers
+            
+        //LINQ statements
     }
 }
+//Basic Requirements
+// ## functionality
+//* display top 3 restaurants by average rating
+//* display all restaurants
+//* should allow more than one method of sorting
+//* display details of a restaurant
+//* display all the reviews of a restaurant
+//* search restaurants(e.g.by partial name), and display all matching results
+//* quit application
